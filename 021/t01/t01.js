f42b42ae5fd6321317ed4c2ef3f0616465b38c78
httptheus <httptheus404@gmail.com>
2022-07-22 20:10:44 -0300

    Commit 22/07/2022

--- 021/t01/t01.js
+++ 021/t01/t01.js
@@ -1,7 +1,7 @@
 /*#021 - Dado - Crie uma interface web 
 que, aleatoriamente, traga um número 
-de 1 a 6. Realize este exercício 
-utilizando os conceitos de Arrays.*/
+de 1 a 6. Exiba imagens de um dado real de 
+acordo com o número tirado.*/
 
 let btnRolar = document.querySelector('#btnRolar')
 
